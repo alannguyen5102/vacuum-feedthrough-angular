a147eb53816ce5e27e0b2292f59f35e317f774c3
alannguyen5102 <alannguyen5102@gmail.com>
2020-07-09 22:56:03 -0700

    Generated fields and preview components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductFieldsComponent } from './product-designer/product-fields/product-fields.component';
+import { ProductPreviewComponent } from './product-designer/product-preview/product-preview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductFieldsComponent,
+    ProductPreviewComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/product-designer/product-fields/product-fields.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-fields',
+  templateUrl: './product-fields.component.html',
+  styleUrls: ['./product-fields.component.css']
+})
+export class ProductFieldsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
