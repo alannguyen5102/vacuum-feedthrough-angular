767ec7a20910f9fcf58afcc0969afc0928510706
alannguyen5102 <alannguyen5102@gmail.com>
2020-07-10 12:49:23 -0700

    adding reactive form to component ts

--- src/app/product-designer/product-fields/product-fields.component.ts
+++ src/app/product-designer/product-fields/product-fields.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Validators, FormBuilder } from '@angular/forms';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-product-fields',
@@ -9,9 +10,29 @@ import { Validators, FormBuilder } from '@angular/forms';
 })
 export class ProductFieldsComponent implements OnInit {
 
-  constructor() { }
+  public productForm: FormGroup;
+  public feedThroughTypes = [];
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.createProductForm();
+    // this.feedThroughTypes = this.getFeedThroughTypes();
+    of(this.getFeedThroughTypes()).subscribe(feedThroughTypes => {
+      this.feedThroughTypes = feedThroughTypes;
+    });
   }
 
+  createProductForm() {
+    this.productForm = this.fb.group({
+      quantity: [1, [Validators.required, Validators.min(0), Validators.max(1000)]],
+      feedThroughType: [, [Validators.required]],
+    });
+  }
+  getFeedThroughTypes() {
+    return [
+      { id: 'kf', name: 'KF Flange' },
+      { id: 'cf', name: 'CF Flange' },
+      { id: 'npt', name: 'Hex Head Fittings'}
+    ];
+  }
 }
