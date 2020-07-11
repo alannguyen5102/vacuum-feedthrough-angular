4c9af4e13793ed6c26c221625ea7a10c9133713d
alannguyen5102 <alannguyen5102@gmail.com>
2020-07-10 22:39:06 -0700

    Adding input to section-label

--- src/app/product-designer/section-label/section-label.component.ts
+++ src/app/product-designer/section-label/section-label.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-section-label',
   templateUrl: './section-label.component.html',
-  styleUrls: ['./section-label.component.css']
+  styleUrls: ['./section-label.component.scss']
 })
 export class SectionLabelComponent implements OnInit {
-
+  
+  @Input() public sectionName: string;
   constructor() { }
 
   ngOnInit(): void {
