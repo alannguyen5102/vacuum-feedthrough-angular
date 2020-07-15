24daaa89639ef3663f14aefc7ff2abd2fefc83e9
alannguyen5102 <alannguyen5102@gmail.com>
2020-07-15 12:10:59 -0700

    switching preview component to scss

--- src/app/product-designer/product-preview/product-preview.component.ts
+++ src/app/product-designer/product-preview/product-preview.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FeedThroughService } from 'src/app/services/feedthrough.service';
 
 @Component({
   selector: 'app-product-preview',
   templateUrl: './product-preview.component.html',
-  styleUrls: ['./product-preview.component.css']
+  styleUrls: ['./product-preview.component.scss']
 })
 export class ProductPreviewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private feedThroughService: FeedThroughService) { }
 
   ngOnInit(): void {
   }
