2be1651e5db0851ff06b4c1c391f086745d0da06
alannguyen5102 <alannguyen5102@gmail.com>
2020-07-15 14:34:59 -0700

    testing out image change

--- src/app/product-designer/product-preview/product-preview.component.ts
+++ src/app/product-designer/product-preview/product-preview.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { FeedThroughService } from 'src/app/services/feedthrough.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product-preview',
   templateUrl: './product-preview.component.html',
   styleUrls: ['./product-preview.component.scss']
 })
-export class ProductPreviewComponent implements OnInit {
 
+export class ProductPreviewComponent implements OnInit {
+  private extraPathName: string = "/assets";
+  public imagePathFeedThrough: string = this.extraPathName + "/images/visuals/kf/qf10-050-lf.PNG";
+  public objectValues$: Observable<any>;
   constructor(private feedThroughService: FeedThroughService) { }
 
   ngOnInit(): void {
+    this.objectValues$ = this.feedThroughService.formValues$;
+    this.objectValues$.subscribe( value => this.changeImage());
+  }
+
+  changeImage() {
+    this.imagePathFeedThrough = this.extraPathName + this.feedThroughService.customerVacuumFeedThrough.getFeedThroughImage();
   }
 
 }
