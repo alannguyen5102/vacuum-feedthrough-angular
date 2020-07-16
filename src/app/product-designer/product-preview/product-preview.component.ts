55627a7640dd68a269c22b9d403873c59a990092
alannguyen5102 <alannguyen5102@gmail.com>
2020-07-15 20:15:26 -0700

    Commenting product-preview code

--- src/app/product-designer/product-preview/product-preview.component.ts
+++ src/app/product-designer/product-preview/product-preview.component.ts
@@ -9,17 +9,30 @@ import { Observable } from 'rxjs';
 })
 
 export class ProductPreviewComponent implements OnInit {
+
+  //String to handle the extra pathing
   private extraPathName: string = "/assets";
+
+  //Two variables to keep the image paths for the feedthrough and wire
   public imagePathFeedThrough: string = this.extraPathName + "/images/visuals/kf/qf16-075-lf.PNG";
   public imagePathWire: string = this.extraPathName + "/images/visuals/leads/A/26G-A-1.PNG";
+
+  //Observable
   public objectValues$: Observable<any>;
+
+  //Injecting FeedThroughService
   constructor(private feedThroughService: FeedThroughService) { }
 
   ngOnInit(): void {
+
+    //Sets product-preview's Observable to FeedThroughService's Observable
+    //When value changes, call changeImage()
     this.objectValues$ = this.feedThroughService.formValues$;
     this.objectValues$.subscribe( value => this.changeImage());
+
   }
 
+  //Calls VacuumFeedThrough class to change the wire and feedthrough images
   changeImage() {
     this.imagePathFeedThrough = this.extraPathName + this.feedThroughService.customerVacuumFeedThrough.getFeedThroughImage();
     this.imagePathWire = this.extraPathName + this.feedThroughService.customerVacuumFeedThrough.getLeadImage();
