156dbc1e2c4d28014e69ee59dbf32cb9631ce171
alannguyen5102 <alannguyen5102@gmail.com>
2020-07-22 22:25:47 -0700

    Fixing bug where # of leads loses required/min

--- src/app/product-designer/product-fields/product-fields.component.ts
+++ src/app/product-designer/product-fields/product-fields.component.ts
@@ -233,7 +233,7 @@ export class ProductFieldsComponent implements OnInit {
   }
   
   updateLeadCountMax(value: number): void {
-    this.productForm.get('vacuumFeedThroughForm.leadCount').setValidators(Validators.max(value));
+    this.productForm.get('vacuumFeedThroughForm.leadCount').setValidators([Validators.required, Validators.min(1), Validators.max(value)]);
     this.productForm.get('vacuumFeedThroughForm.leadCount').updateValueAndValidity({onlySelf: true, emitEvent: false});
     this.maxLeadOutput = value;
   }
